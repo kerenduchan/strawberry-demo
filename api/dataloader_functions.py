40a12a7b2b2980faf4369ba622cccd49161e028c
Keren Duchan <keren.duchan@gmail.com>
2023-01-18 17:40:16 +0200

    typed dataloader function builders

--- api/dataloader_functions.py
+++ api/dataloader_functions.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Any, TypeVar
 from strawberry.dataloader import DataLoader
 import db.schema
 from db.session import session_maker
@@ -7,46 +7,51 @@ from sqlalchemy import select
 
 def get_all_dataloaders() -> Dict[str, DataLoader]:
     return {
-        "author_by_id": DataLoader(load_fn=_get_authors_by_ids),
-        "books_by_author_id": DataLoader(load_fn=_get_books_by_author_ids),
+        "author_by_id":
+            DataLoader(load_fn=_build_id_fn(db.schema.Author)),
+
+        "books_by_author_id":
+            DataLoader(load_fn=_build_column_fn(db.schema.Book, "author_id")),
     }
 
 
-async def _get_authors_by_ids(ids: List[str]) -> List[db.schema.Author]:
-    return await _get_by_ids('Author', ids)
+DbTableClass = TypeVar("DbTableClass")
 
 
-async def _get_books_by_author_ids(ids: List[int]) -> List[db.schema.Author]:
-    return await _get_objects_by_column('Book', 'author_id', ids)
+def _build_id_fn(class_: DbTableClass):
+    """ return a dataloader function for getting all objects of
+    the given class by the given list of IDs """
+
+    async def get_by_ids(ids: List[int]) -> List[class_]:
+        async with session_maker() as session:
+            sql = select(class_).where(class_.id.in_(ids))
+            res = await session.execute(sql)
+            recs = res.scalars().all()
+        # order the result in the same order as the given ids list
+        recs_dict = {r.id: r for r in recs}
+        ordered = []
+        for id_ in ids:
+            ordered.append(recs_dict.get(id_))
+        return ordered
+
+    return get_by_ids
 
 
-async def _get_by_ids(class_name: str, ids: List[str]) -> List[Any]:
-    class_ = getattr(db.schema, class_name)
-    async with session_maker() as session:
-        sql = select(class_).where(class_.id.in_(ids))
-        res = await session.execute(sql)
-        recs = res.scalars().all()
+def _build_column_fn(class_: DbTableClass, column_name: str):
+    """ return a dataloader function for getting all objects of
+    the given class whose column value is in the given list of values """
 
-    # order the result in the same order as the given ids list
-    recs_dict = {r.id: r for r in recs}
-    ordered = []
-    for id_ in ids:
-        ordered.append(recs_dict.get(id_))
-    return ordered
+    async def get_by_column(values: List[str]) -> List[class_]:
+        column = getattr(class_, column_name)
+        async with session_maker() as session:
+            sql = select(class_).where(column.in_(values))
+            res = await session.execute(sql)
+            recs = res.scalars().all()
 
+            # order the results in the same order as the given values list
+            groups = {v: [] for v in values}
+            for rec in recs:
+                groups[getattr(rec, column_name)].append(rec)
+            return list(groups.values())
 
-async def _get_objects_by_column(
-        class_name: str, column_name: str, values: List[Any]) \
-        -> List[Any]:
-    class_ = getattr(db.schema, class_name)
-    column = getattr(class_, column_name)
-    async with session_maker() as session:
-        sql = select(class_).where(column.in_(values))
-        res = await session.execute(sql)
-        recs = res.scalars().all()
-
-        # order the results in the same order as the given values list
-        groups = {v: [] for v in values}
-        for rec in recs:
-            groups[getattr(rec, column_name)].append(rec)
-        return list(groups.values())
+    return get_by_column
