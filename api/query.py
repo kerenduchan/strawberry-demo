0246533954de9e804c94365d1a27dfe6a174d34d
Keren Duchan <keren.duchan@gmail.com>
2023-01-19 08:49:54 +0200

    move query resolvers to a separate file

--- api/query.py
+++ api/query.py
@@ -1,62 +1,18 @@
-from typing import Dict, TypeVar
 import strawberry
 from api.author import Author
 from api.book import Book
 from api.pagination_window import PaginationWindow
-from db.session import session_maker
-import db.schema
-import db.utils
+import api.resolvers.query
 
 
 @strawberry.type
 class Query:
 
-    @strawberry.field
-    async def books(self,
-                    order_by: str | None = "title",
-                    limit: int = 100,
-                    offset: int = 0,
-                    title: str | None = None) -> PaginationWindow[Book]:
-        filters = {}
-        if title:
-            filters['title'] = title
+    books: PaginationWindow[Book] = strawberry.field(
+        resolver=api.resolvers.query.books,
+        description="get books")
 
-        resolve = _get_resolver(db.schema.Book, Book)
-        return await resolve(order_by, limit, offset, filters)
+    authors: PaginationWindow[Author] = strawberry.field(
+        resolver=api.resolvers.query.authors,
+        description="get authors")
 
-    @strawberry.field
-    async def authors(self,
-                      order_by: str | None = "name",
-                      limit: int = 100,
-                      offset: int = 0,
-                      name: str | None = None) -> PaginationWindow[Author]:
-        filters = {}
-
-        if name:
-            filters['name'] = name
-
-        resolve = _get_resolver(db.schema.Author, Author)
-        return await resolve(order_by, limit, offset, filters)
-
-
-DbClass = TypeVar("DbClass")
-ApiClass = TypeVar("ApiClass")
-
-
-def _get_resolver(db_class: DbClass, api_class: ApiClass | Author):
-
-    async def resolve(order_by: str,
-                      limit: int = 100,
-                      offset: int = 0,
-                      filters: Dict[str, str] | None = None) \
-            -> PaginationWindow[api_class]:
-
-        async with session_maker() as session:
-            window = await db.utils.get(
-                session, db_class, order_by, limit, offset, filters)
-
-            return PaginationWindow[api_class](
-                items=[api_class.from_db(item) for item in window.items],
-                total_items_count=window.total_items_count)
-
-    return resolve
---
+++ api/resolvers/query.py
@@ -0,0 +1,57 @@
+from typing import Dict, TypeVar
+from db.session import session_maker
+from api.pagination_window import PaginationWindow
+from api.book import Book
+from api.author import Author
+import db.schema
+import db.utils
+
+
+async def books(
+        order_by: str | None = "title",
+        limit: int = 100,
+        offset: int = 0,
+        title: str | None = None) -> PaginationWindow[Book]:
+    filters = {}
+    if title:
+        filters['title'] = title
+
+    resolve = _get_resolver(db.schema.Book, Book)
+    return await resolve(order_by, limit, offset, filters)
+
+
+async def authors(
+        order_by: str | None = "name",
+        limit: int = 100,
+        offset: int = 0,
+        name: str | None = None) -> PaginationWindow[Author]:
+        filters = {}
+
+        if name:
+            filters['name'] = name
+
+        resolve = _get_resolver(db.schema.Author, Author)
+        return await resolve(order_by, limit, offset, filters)
+
+
+DbClass = TypeVar("DbClass")
+ApiClass = TypeVar("ApiClass")
+
+
+def _get_resolver(db_class: DbClass, api_class: ApiClass | Author):
+
+    async def resolve(order_by: str,
+                      limit: int = 100,
+                      offset: int = 0,
+                      filters: Dict[str, str] | None = None) \
+            -> PaginationWindow[api_class]:
+
+        async with session_maker() as session:
+            window = await db.utils.get(
+                session, db_class, order_by, limit, offset, filters)
+
+            return PaginationWindow[api_class](
+                items=[api_class.from_db(item) for item in window.items],
+                total_items_count=window.total_items_count)
+
+    return resolve
