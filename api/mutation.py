c3fedd825c0ea15ba1fddeaeee22b44eec19542f
Keren Duchan <keren.duchan@gmail.com>
2023-01-19 09:15:20 +0200

    move files to api/utils dir

--- api/mutation.py
+++ api/mutation.py
@@ -2,32 +2,32 @@ import strawberry
 from api.book import Book
 from api.author import Author
 from api.count import Count
-import api.resolvers.mutation
+import api.utils.resolvers.mutation
 
 
 @strawberry.type
 class Mutation:
 
     create_author: Author = strawberry.mutation(
-        resolver=api.resolvers.mutation.create_author,
+        resolver=api.utils.resolvers.mutation.create_author,
         description="create an author")
 
     update_author: Author = strawberry.mutation(
-        resolver=api.resolvers.mutation.update_author,
+        resolver=api.utils.resolvers.mutation.update_author,
         description="update an author")
 
     delete_author: Count = strawberry.mutation(
-        resolver=api.resolvers.mutation.delete_author,
+        resolver=api.utils.resolvers.mutation.delete_author,
         description="delete an author")
 
     create_book: Book = strawberry.mutation(
-        resolver=api.resolvers.mutation.create_book,
+        resolver=api.utils.resolvers.mutation.create_book,
         description="create a book")
 
     update_book: Book = strawberry.mutation(
-        resolver=api.resolvers.mutation.update_book,
+        resolver=api.utils.resolvers.mutation.update_book,
         description="update a book")
 
     delete_book: Count = strawberry.mutation(
-        resolver=api.resolvers.mutation.delete_book,
+        resolver=api.utils.resolvers.mutation.delete_book,
         description="delete a book")
--- api/query.py
+++ api/query.py
@@ -2,17 +2,17 @@ import strawberry
 from api.author import Author
 from api.book import Book
 from api.pagination_window import PaginationWindow
-import api.resolvers.query
+import api.utils.resolvers.query
 
 
 @strawberry.type
 class Query:
 
     books: PaginationWindow[Book] = strawberry.field(
-        resolver=api.resolvers.query.books,
+        resolver=api.utils.resolvers.query.books,
         description="get books")
 
     authors: PaginationWindow[Author] = strawberry.field(
-        resolver=api.resolvers.query.authors,
+        resolver=api.utils.resolvers.query.authors,
         description="get authors")
 
